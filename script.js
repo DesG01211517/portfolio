f5485d50234cf4df73e08be2c463a5e505242aff
DesG01211517 <desmongarrison2@gmail.com>
2024-03-19 21:30:44 -0400

    test Java buttons complete

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+//let scootRental = document.getElementById("blackScooter"); 
+
+blackScoot.onclick = function () {
+    alert("You clicked the Rent Me button!");
+}
+//console.log(blackScoot);
+
+redScoot.onclick = function () {
+    alert("You clicked the Rent Me button!");
+}
+
+blueScoot.onclick = function () {
+    alert("You clicked the Rent Me button!");
+}
+
+grayScoot.onclick = function () {
+    alert("You clicked the Rent Me button!");
+}
