09f9078422f21adb048557526b481c730c86643d
DesG01211517 <desmongarrison2@gmail.com>
2024-03-19 22:49:43 -0400

    store complete

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 let allElms = [
     redScoot, blackScoot, blueScoot, grayScoot
 ]
-
 blackScoot.onclick = function () {
     blackScoot.textContent = "RESERVED";
-    //alert("You clicked the Rent Me button!");
-}
-
+    alert("You clicked the Rent Me button!");}
 redScoot.onclick = function () {
     redScoot.textContent = "RESERVED";
-    //alert("You clicked the Rent Me button!");
-}
-
+    alert("You clicked the Rent Me button!");}
 blueScoot.onclick = function () {
     blueScoot.textContent = "RESERVED";
-    //alert("You clicked the Rent Me button!");
-}
-
+    alert("You clicked the Rent Me button!");}
 grayScoot.onclick = function () {
     grayScoot.textContent = "RESERVED";
-    //alert("You clicked the Rent Me button!");
-}
-
+    alert("You clicked the Rent Me button!");}
 resetButton.onclick = function () {
     for (let i=0; i<4; i++) {
         let a = allElms[i];
         a.textContent = "RENT ME!!";
     }
 }
-//console.log(a)
+
+
+
+
+
+
+
+
